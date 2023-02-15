4629f4d02e0422c311a7877ba316dde222b23c9e
Maurycy Markowski <maumar@microsoft.com>
2023-02-14 18:42:01 -0800

    Gf

--- test/EFCore.Sqlite.FunctionalTests/SqliteComplianceTest.cs
+++ test/EFCore.Sqlite.FunctionalTests/SqliteComplianceTest.cs
@@ -8,7 +8,6 @@ public class SqliteComplianceTest : RelationalComplianceTestBase
     protected override ICollection<Type> IgnoredTestBases { get; } = new HashSet<Type>
     {
         typeof(FromSqlSprocQueryTestBase<>),
-        typeof(JsonUpdateTestBase<>),
         typeof(SqlExecutorTestBase<>),
         typeof(UdfDbFunctionTestBase<>),
         typeof(TPCRelationshipsQueryTestBase<>), // internal class is added
